ee5f33394cba90334944fa345bd1542b4e013f21
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2023-04-26 14:02:06 +0200

    Actually fix group id 😅

--- parent/build.gradle.kts
+++ parent/build.gradle.kts
@@ -2,11 +2,6 @@ plugins {
     id("io.micronaut.build.internal.maven-pom")
 }
 
-// Intentionally set to an "old" groupId so that
-// the parent POM is published at the same GAV coordinates
-// as the Micronaut 3 version
-group = "io.micronaut"
-
 pom {
     catalogPropertyPrefix.set("parent")
     includeBomPropertiesStringListProperty.set(listOf(
