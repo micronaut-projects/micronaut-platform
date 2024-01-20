87bc72908c57558276dbe51e2528082600cbea2b
Tim Yates <tim.yates@gmail.com>
2024-01-20 06:23:00 +0000

    Cleanup the bom silencing from micronaut-tracing (#1187)
    * Cleanup the bom silencing from micronaut-tracing
    
    * Kick the CLA action...

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -61,16 +61,11 @@ micronautBom {
             "io.projectreactor:reactor-bom",
             setOf("io.projectreactor", "org.reactivestreams")
         )
-        dependencies.add("io.zipkin.brave:brave-bom:5.17.0")
+
         dependencies.add("io.zipkin.reporter2:zipkin-reporter-bom:2.16.3")
-
-        dependencies.add("io.opentelemetry:opentelemetry-bom:1.31.0")
-        dependencies.add("io.opentelemetry:opentelemetry-bom-alpha:1.31.0-alpha")
-
         dependencies.add("io.opentelemetry:opentelemetry-bom:1.31.0")
         dependencies.add("io.opentelemetry:opentelemetry-bom-alpha:1.31.0-alpha")
         dependencies.add("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom:1.31.0")
-        dependencies.add("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:1.31.0-alpha")
 
         // This is in the micrometer BOM for version 1.11.5, but the module is never published
         // See https://github.com/micrometer-metrics/micrometer/issues/4350
