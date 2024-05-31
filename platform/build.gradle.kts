e76cc5c2fb365029fc518e93b828d64518476f21
Graeme Rocher <graeme.rocher@oracle.com>
2024-05-31 12:47:01 +0200

    fix PR

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -67,11 +67,11 @@ micronautBom {
         )
 
         dependencies.add("io.zipkin.reporter2:zipkin-reporter-bom:3.4.0")
-        dependencies.add("io.zipkin.brave:brave-instrumentation-benchmarks:6.0.2")
+        dependencies.add("io.zipkin.brave:brave-instrumentation-benchmarks:6.0.3")
 
-        dependencies.add("io.opentelemetry:opentelemetry-bom:1.36.0")
-        dependencies.add("io.opentelemetry:opentelemetry-bom-alpha:1.36.0-alpha")
-        dependencies.add("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom:1.33.1")
+        dependencies.add("io.opentelemetry:opentelemetry-bom:1.38.0")
+        dependencies.add("io.opentelemetry:opentelemetry-bom-alpha:1.38.0-alpha")
+        dependencies.add("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom:1.33.3")
 
         // This is in the micrometer BOM for version 1.12.2, but the module is never published
         // See https://github.com/micrometer-metrics/micrometer/issues/4606
