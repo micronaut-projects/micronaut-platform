d4b9e2bc3e09026de6599a6c979aab9a11c5e2f8
Sergio del Amo <sergio.delamo@softamo.com>
2024-08-07 15:48:12 +0200

    opentelemetry-bom-alpha 1.40.0-alpha

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -70,7 +70,7 @@ micronautBom {
         dependencies.add("io.zipkin.brave:brave-instrumentation-benchmarks:6.0.3")
 
         dependencies.add("io.opentelemetry:opentelemetry-bom:1.40.0")
-        dependencies.add("io.opentelemetry:opentelemetry-bom-alpha:1.38.0-alpha")
+        dependencies.add("io.opentelemetry:opentelemetry-bom-alpha:1.40.0-alpha")
         dependencies.add("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom:1.33.5")
 
         // This is in the micrometer BOM for version 1.12.2, but the module is never published
